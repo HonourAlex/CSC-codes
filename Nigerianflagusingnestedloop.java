722e1e30e5e3bd223e8dc3489ab8c58af748d574
HonourAlex <alexander.honour@lmu.edu.ng>
2025-01-20 09:22:01 +0100

    Add files via upload

---
+++ Nigerianflagusingnestedloop.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class Nigerianflagusingnestedloop {
+
+
+	    public static void main(String[] args) {
+	        int rows = 4;
+	        
+	        for (int i = 0; i < rows; i++) {
+	            for (int j = 0; j < 1; j++) {
+	                System.out.print("**** ");
+	                System.out.print("==== ");
+	                System.out.print("****");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+}
